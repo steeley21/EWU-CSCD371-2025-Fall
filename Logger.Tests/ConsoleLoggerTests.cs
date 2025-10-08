31fa9ceec7296a22d8c6e92588165cf56d997424
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-07 19:34:34 -0700

    Console Logger and Console Logger Tests

---
+++ Logger.Tests/ConsoleLoggerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests
+{
+    [TestClass]
+    public class ConsoleLoggerTests
+    {
+        private StringWriter _stringWriter = null!;
+        private TextWriter _output = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _stringWriter = new StringWriter();
+            _output = Console.Out;
+            Console.SetOut(_stringWriter);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Console.SetOut(_output);
+            _stringWriter.Dispose();
+        }
+
+        [TestMethod]
+        public void ConsoleLogger_Instantiate_Success()
+        {
+            //Arrange
+            //Act
+            var logger = new ConsoleLogger();
+            //Assert
+            Assert.IsNotNull(logger);
+            Assert.IsInstanceOfType<ConsoleLogger>(logger);
+        }
+
+        [TestMethod]
+        public void Log_WhenCalled_WritesExpectedOutputToConsole()
+        {
+            var logger = new ConsoleLogger
+            {
+                ClassName = nameof(ConsoleLoggerTests)
+            };
+
+            logger.Log(LogLevel.Information, "message");
+
+            string output = _stringWriter.ToString();
+            Assert.IsTrue(output.Contains("ConsoleLoggerTests"));
+            Assert.IsTrue(output.Contains("Information"));
+            Assert.IsTrue(output.Contains("message"));
+        }
+
+    }
+}
---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class ConsoleLogger : BaseLogger
+    {
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string timestamp = DateTime.Now.ToString("M/d/yyyy h:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture);
+            string className = ClassName;
+            Console.WriteLine($"{timestamp} {className} {logLevel}: {message}");
+        }
+    }
+}
