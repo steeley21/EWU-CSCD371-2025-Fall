ace44b594593748b705f1fc6838a35d1a12c7dbe
steeley21 <ksteele5@ewu.edu>
2025-10-10 16:19:05 -0700

    added using System and ConsoleOutput

---
+++ CanHazFunny/CanHazFunny/ConsoleOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CanHazFunny;
 
 public class Jester
