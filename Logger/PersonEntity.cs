5f7ae39282c877764f67acc3595c1cf31bcd8545
steeley21 <ksteele5@ewu.edu>
2025-10-17 19:57:16 -0700

    created PersonEntity abstract

---
+++ Logger/PersonEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public abstract class PersonEntity : Entity
+    {
+        protected PersonEntity(FullName fullName)
+        {
+            FullName = fullName;
+        }
+
+        public FullName FullName { get; init; }
+
+        protected string PersonDisplayName => FullName.ToString();
+    }
+}
