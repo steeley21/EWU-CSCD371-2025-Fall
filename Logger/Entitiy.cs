54ab7f287a1a4882cc2272f25134427166186f2b
steeley21 <ksteele5@ewu.edu>
2025-10-17 19:40:59 -0700

    fixed entity

--- Logger/Entitiy.cs
+++
@@ -1,8 +0,0 @@
-namespace Logger;
-
-public abstract class Entitiy : IEntity
-{
-    public Guid Id { get; init; } = Guid.NewGuid();
-    public required string Name { get; init; }
-
-}
---
+++ Logger/Entity.cs
@@ -0,0 +1,8 @@
+namespace Logger;
+
+public abstract class Entity : IEntity
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public abstract string Name { get; }
+
+}
